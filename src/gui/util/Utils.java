0b32969085862f526a3fda6bbe9190bb973a3d7c
mateuslgomes <mateuslgomes021@gmail.com>
2022-05-24 15:31:40 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -7,6 +7,19 @@ import javafx.stage.Stage;
 public class Utils {
 
 	public static Stage currentStage(ActionEvent event) {
-		return (Stage) ((Node) event.getSource()).getScene().getWindow();
+		try {
+			return (Stage) ((Node) event.getSource()).getScene().getWindow();
+		}
+		catch (NumberFormatException e) {
+			return null;
+		}
+	}
+	
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
 	}
 }
